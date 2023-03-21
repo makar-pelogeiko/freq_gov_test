c54fd3ca899be58b1015219108edd0d08b9c0680
makar-pelogeiko <m.pelogeiko@mail.ru>
2023-03-21 20:54:21 +0300

    [anti hotplug added]

--- change_freq_governor.py
+++ change_freq_governor.py
@@ -1,4 +1,5 @@
 from testing_core.freq_gov_changer import FreqGovChanger
+from testing_core.phone_prepare import Preparer
 import config
 import sys
 
@@ -8,6 +9,10 @@ if __name__ == "__main__":
         gov_name = sys.argv[1]
 
         print(f'try to set new freq governor: {gov_name}')
-
         changer = FreqGovChanger(config.path_adb)
+
+        if config.need_anti_hotplug:
+            preparer = Preparer(*config.standard_test_args)
+            preparer.anti_hotplug()
+
         changer.change_governor(gov_name)
